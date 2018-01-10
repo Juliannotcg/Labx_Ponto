7dd5b912650286d543b8a3b23c83c43815869fa4
Julianno Teixeira Cortez Garcia <juliannotcg@hotmail.com>
2018-01-09 23:17:26 -0200

    Ajustes

--- LabxPonto_View/Properties/Resources.Designer.cs
+++ LabxPonto_View/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace LabxPonto_View.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap ePonto {
+            get {
+                object obj = ResourceManager.GetObject("ePonto", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap if_new_24_103173 {
             get {
                 object obj = ResourceManager.GetObject("if_new-24_103173", resourceCulture);
@@ -116,6 +126,16 @@ namespace LabxPonto_View.Properties {
         internal static System.Drawing.Bitmap if_user_delete_outline_216492 {
             get {
                 object obj = ResourceManager.GetObject("if_user-delete-outline_216492", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Logo1 {
+            get {
+                object obj = ResourceManager.GetObject("Logo1", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
