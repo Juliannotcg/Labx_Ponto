f5b2997a7a2e76bf067df4745a58fc40b9535fb7
Julianno Garcia <julianno@p21sistemas.com.br>
2018-01-24 11:01:21 -0200

    Ajustes

--- LabxPonto_View/Views/Relatórios/frmRelatorios.Designer.cs
+++ LabxPonto_View/Views/Relatórios/frmRelatorios.Designer.cs
@@ -43,30 +43,29 @@
             // 
             // btnCancelar
             // 
-            this.btnCancelar.Location = new System.Drawing.Point(383, 238);
-            this.btnCancelar.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.btnCancelar.Location = new System.Drawing.Point(287, 193);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnCancelar.TabIndex = 4;
             this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
             // btnSalvar
             // 
-            this.btnSalvar.Location = new System.Drawing.Point(252, 238);
-            this.btnSalvar.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.btnSalvar.Location = new System.Drawing.Point(189, 193);
+            this.btnSalvar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSalvar.TabIndex = 3;
             this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
             // 
             // pbLogo
             // 
-            this.pbLogo.Location = new System.Drawing.Point(7, 261);
-            this.pbLogo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pbLogo.Location = new System.Drawing.Point(5, 212);
+            this.pbLogo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             // 
             // metroLabel29
             // 
             this.metroLabel29.AutoSize = true;
-            this.metroLabel29.Location = new System.Drawing.Point(28, 160);
-            this.metroLabel29.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.metroLabel29.Location = new System.Drawing.Point(21, 130);
             this.metroLabel29.Name = "metroLabel29";
-            this.metroLabel29.Size = new System.Drawing.Size(56, 20);
+            this.metroLabel29.Size = new System.Drawing.Size(55, 19);
             this.metroLabel29.TabIndex = 65;
             this.metroLabel29.Text = "Período";
             this.metroLabel29.UseCustomBackColor = true;
@@ -75,10 +74,9 @@
             // metroLabel2
             // 
             this.metroLabel2.AutoSize = true;
-            this.metroLabel2.Location = new System.Drawing.Point(241, 199);
-            this.metroLabel2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.metroLabel2.Location = new System.Drawing.Point(181, 162);
             this.metroLabel2.Name = "metroLabel2";
-            this.metroLabel2.Size = new System.Drawing.Size(16, 20);
+            this.metroLabel2.Size = new System.Drawing.Size(16, 19);
             this.metroLabel2.TabIndex = 67;
             this.metroLabel2.Text = "a";
             this.metroLabel2.UseCustomBackColor = true;
@@ -87,10 +85,9 @@
             // metroLabel26
             // 
             this.metroLabel26.AutoSize = true;
-            this.metroLabel26.Location = new System.Drawing.Point(27, 84);
-            this.metroLabel26.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.metroLabel26.Location = new System.Drawing.Point(20, 68);
             this.metroLabel26.Name = "metroLabel26";
-            this.metroLabel26.Size = new System.Drawing.Size(33, 20);
+            this.metroLabel26.Size = new System.Drawing.Size(33, 19);
             this.metroLabel26.TabIndex = 91;
             this.metroLabel26.Text = "CPF";
             this.metroLabel26.UseCustomBackColor = true;
@@ -102,20 +99,20 @@
             // 
             // dtDataIni
             // 
-            this.dtDataIni.Location = new System.Drawing.Point(27, 187);
-            this.dtDataIni.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.dtDataIni.MinimumSize = new System.Drawing.Size(0, 30);
+            this.dtDataIni.Location = new System.Drawing.Point(20, 152);
+            this.dtDataIni.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dtDataIni.MinimumSize = new System.Drawing.Size(0, 29);
             this.dtDataIni.Name = "dtDataIni";
-            this.dtDataIni.Size = new System.Drawing.Size(200, 30);
+            this.dtDataIni.Size = new System.Drawing.Size(151, 30);
             this.dtDataIni.TabIndex = 1;
             // 
             // dtDataFim
             // 
-            this.dtDataFim.Location = new System.Drawing.Point(280, 187);
-            this.dtDataFim.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.dtDataFim.MinimumSize = new System.Drawing.Size(0, 30);
+            this.dtDataFim.Location = new System.Drawing.Point(210, 152);
+            this.dtDataFim.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dtDataFim.MinimumSize = new System.Drawing.Size(0, 29);
             this.dtDataFim.Name = "dtDataFim";
-            this.dtDataFim.Size = new System.Drawing.Size(200, 30);
+            this.dtDataFim.Size = new System.Drawing.Size(151, 30);
             this.dtDataFim.TabIndex = 2;
             // 
             // txtCPF
@@ -124,19 +121,19 @@
             // 
             // 
             this.txtCPF.CustomButton.Image = null;
-            this.txtCPF.CustomButton.Location = new System.Drawing.Point(229, 2);
-            this.txtCPF.CustomButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtCPF.CustomButton.Location = new System.Drawing.Point(127, 1);
+            this.txtCPF.CustomButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtCPF.CustomButton.Name = "";
-            this.txtCPF.CustomButton.Size = new System.Drawing.Size(31, 31);
+            this.txtCPF.CustomButton.Size = new System.Drawing.Size(20, 22);
             this.txtCPF.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
             this.txtCPF.CustomButton.TabIndex = 1;
             this.txtCPF.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
             this.txtCPF.CustomButton.UseSelectable = true;
             this.txtCPF.CustomButton.Visible = false;
             this.txtCPF.Lines = new string[0];
-            this.txtCPF.Location = new System.Drawing.Point(28, 106);
-            this.txtCPF.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txtCPF.MaxLength = 11;
+            this.txtCPF.Location = new System.Drawing.Point(21, 86);
+            this.txtCPF.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtCPF.MaxLength = 14;
             this.txtCPF.Name = "txtCPF";
             this.txtCPF.PasswordChar = '\0';
             this.txtCPF.ScrollBars = System.Windows.Forms.ScrollBars.None;
@@ -144,7 +141,7 @@
             this.txtCPF.SelectionLength = 0;
             this.txtCPF.SelectionStart = 0;
             this.txtCPF.ShortcutsEnabled = true;
-            this.txtCPF.Size = new System.Drawing.Size(263, 36);
+            this.txtCPF.Size = new System.Drawing.Size(197, 29);
             this.txtCPF.TabIndex = 0;
             this.txtCPF.UseSelectable = true;
             this.txtCPF.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
@@ -153,18 +150,18 @@
             // 
             // frmRelatorios
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(537, 304);
+            this.ClientSize = new System.Drawing.Size(403, 247);
             this.Controls.Add(this.txtCPF);
             this.Controls.Add(this.dtDataFim);
             this.Controls.Add(this.dtDataIni);
             this.Controls.Add(this.metroLabel26);
             this.Controls.Add(this.metroLabel2);
             this.Controls.Add(this.metroLabel29);
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmRelatorios";
-            this.Padding = new System.Windows.Forms.Padding(36, 91, 36, 31);
+            this.Padding = new System.Windows.Forms.Padding(27, 74, 27, 25);
             this.Text = "Relatórios";
             this.Controls.SetChildIndex(this.pbLogo, 0);
             this.Controls.SetChildIndex(this.btnSalvar, 0);
